17b99c1587963ec468808855dfc83111fa336b22
Patryk 74173 <gdx164734@student.gdansk.merito.pl>
2024-03-09 14:33:37 +0100

    Edycja nawigacji oraz footera

--- components/Footer/index.vue
+++ components/Footer/index.vue
@@ -1,11 +1,88 @@
+<script>
+</script>
 <template>
     <footer class="footer">
-        <div class="content has-text-centered">
-            <p>
-                <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is licensed
-                <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
-                is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
-            </p>
+        <div class="footer-section">
+            <h3><b>PORADNIK ZAKUPOWY</b></h3>
+            <ul class="footer-list underline">
+                <li class="footer-list-item">
+                    <NuxtLink class="navbar-item" to="/">
+                        Metody płatności</NuxtLink>
+                </li>
+                <li class="footer-list-item">
+                    <NuxtLink class="navbar-item" to="/">
+                        Formy dostawy</NuxtLink>
+                </li>
+                <li class="footer-list-item">
+                    <NuxtLink class="navbar-item" to="/">
+                        Reklamacje</NuxtLink>
+                </li>
+                <li class="footer-list-item">
+                    <NuxtLink class="navbar-item" to="/">
+                        Zwroty, wymiana</NuxtLink>
+                </li>
+                <li class="footer-list-item">
+                    <NuxtLink class="navbar-item" to="/">
+                        Kontakt</NuxtLink>
+                </li>
+                <li class="footer-list-item">
+                    <NuxtLink class="navbar-item" to="/">
+                        Testowy link</NuxtLink>
+                </li>
+            </ul>
         </div>
+
+        <div class="footer-section">
+            <h3><b>ZASADY I REGULAMINY</b></h3>
+            <ul class="footer-list underline">
+                <li class="footer-list-item">
+                    <NuxtLink class="navbar-item" to="/">
+                        Regulamin sklepu
+                    </NuxtLink>
+                </li>
+                <li class="footer-list-item">
+                    <NuxtLink class="navbar-item" to="/">Regulamin karty stałego klienta</NuxtLink>
+                </li>
+                <li class="footer-list-item">
+                    <NuxtLink class="navbar-item" to="/">Regulamin karty podarunkowej</NuxtLink>
+                </li>
+                <li class="footer-list-item">
+                    <NuxtLink class="navbar-item" to="/">Saldo karty podarunkowej</NuxtLink>
+                </li>
+            </ul>
+        </div>
+
+        <div class="footer-section">
+            <h3><b>O NAS</b></h3>
+            <ul class="footer-list underline">
+                <li class="footer-list-item">
+                    <NuxtLink class="navbar-item" to="/">
+                        Regulamin sklepu
+                    </NuxtLink>
+                </li>
+                <li class="footer-list-item">
+                    <NuxtLink class="navbar-item" to="/">Regulamin karty stałego klienta</NuxtLink>
+                </li>
+                <li class="footer-list-item">
+                    <NuxtLink class="navbar-item" to="/">Regulamin karty podarunkowej</NuxtLink>
+                </li>
+                <li class="footer-list-item">
+                    <NuxtLink class="navbar-item" to="/">Saldo karty podarunkowej</NuxtLink>
+                </li>
+            </ul>
+        </div>
+        <div class="footer-sections">
+            <!-- Miejsce na sociale -->
+        </div>
+
     </footer>
+    <div class="bottom-bar">
+        <div class="copyright">
+            ©2023 CLICK FASHION. All rights reserved.
+        </div>
+        <img class="scroll-to-top" src="assets/arrowUp.png">
+        <div class="madeBy">
+            <a href="google.com">Proudly made by</a>
+        </div>
+    </div>
 </template>
