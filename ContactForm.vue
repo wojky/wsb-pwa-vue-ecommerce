8ac982a89cf33f3ef26424a48dff63373f24ef4f
Filip Walczak <101400986+info-ext@users.noreply.github.com>
2024-04-12 19:02:00 +0200

    Formularz kontaktowy
    Pierwsza czesc formularza

---
+++ ContactForm.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="contact-form">
+      <h1>Napisz do nas</h1>
+      <form @submit.prevent="submitForm">
+        <label for="name">
+          Imię*
+          <input type="text" id="name" v-model="form.name" required>
+        </label>
+        <label for="email">
+          E-mail*
+          <input type="email" id="email" v-model="form.email" required>
+        </label>
+        <label for="phone">
+          Numer telefonu
+          <input type="tel" id="phone" v-model="form.phone">
+        </label>
+        <label for="message">
+          Co masz na myśli?*
+          <textarea id="message" v-model="form.message" required></textarea>
+        </label>
+        <button type="submit">Wyślij</button>
+      </form>
+    </div>
+  </template>
